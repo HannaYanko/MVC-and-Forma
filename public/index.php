fa7bf0565346d055f75366cb7e3341b5309496d1
HannaYanko <annayanko04@gmail.com>
2023-11-24 13:45:09 +0200

    Docker

--- public/index.php
+++ public/index.php
@@ -1,2 +1,15 @@
 <?php
-echo "hi";
+require 'vendor/autoload.php';
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('path/to/your/log/file.log', Logger::WARNING));
+
+
+$log->warning('Попередження');
+
+
+
