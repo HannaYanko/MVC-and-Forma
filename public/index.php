6fd6e2c44884c9ddedf7820f88f99880f854f72f
HannaYanko <annayanko04@gmail.com>
2023-11-26 15:09:01 +0200

    Docker

--- public/index.php
+++ public/index.php
@@ -1,15 +1,12 @@
 <?php
-require 'vendor/autoload.php';
+require __DIR__. '/../vendor/autoload.php';
 
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
-
 $log = new Logger('name');
-$log->pushHandler(new StreamHandler('path/to/your/log/file.log', Logger::WARNING));
-
+$log->pushHandler(new StreamHandler('app.log', Logger::WARNING));
 
 $log->warning('Попередження');
-
-
-
+$logContents = file_get_contents('app.log');
+echo $logContents;
